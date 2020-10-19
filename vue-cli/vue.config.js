c54a441ebd15dff0479cff33a565bb5f9deaad5f
hel1yeah <hel1_yeah@ukr.net>
2020-10-19 22:58:19 +0300

    021 Настройка конфига

---
+++ vue-cli/vue.config.js
@@ -0,0 +1,10 @@
+// vue.config.js
+module.exports = {
+  // настройки...
+  publicPath: process.env.NODE_ENV === 'production'
+    ? ''
+    : '/',
+  assetsDir: 'assets',
+  productionSourceMap: false,
+  filenameHashing: false,
+}
