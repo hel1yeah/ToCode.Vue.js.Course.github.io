9a2d1cb8c5023e3ada7f0f7e01641b78d19e3129
hel1yeah <hel1_yeah@ukr.net>
2020-10-20 21:56:06 +0300

    022 Обновление пакетов

--- vue-cli/vue.config.js
+++ vue-cli/vue.config.js
@@ -2,9 +2,13 @@
 module.exports = {
   // настройки...
   publicPath: process.env.NODE_ENV === 'production'
-    ? ''
+
+    ? '/'
     : '/',
+  
   assetsDir: 'assets',
+
   productionSourceMap: false,
+
   filenameHashing: false,
 }
