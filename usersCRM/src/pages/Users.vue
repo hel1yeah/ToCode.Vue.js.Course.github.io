89157f1a954acb8c188f518ab68ec8a97d9a8486
hel1yeah <hel1_yeah@ukr.net>
2020-12-07 23:34:58 +0200

    save 95 lessons

--- usersCRM/src/pages/Users.vue
+++ usersCRM/src/pages/Users.vue
@@ -2,9 +2,48 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-        <h1>Страница пользователей</h1>
+
+        <!-- table -->
+        <table>
+
+        <!-- head -->
+          <thead>
+            <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Пол</th>
+            </tr>
+          </thead>
+
+          <!-- body -->
+          <tbody>
+            <tr v-for="user in users" :key="user.id">
+            <td> {{user.name}} </td>
+            <td> {{user.age}} </td>
+            <td> {{user.gender}} </td>
+            </tr>
+          </tbody>
+        </table>
+
       </div>
     </section>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        
+      ],
+    };
+  },
+  created() {
+    this.users = [
+      { id: 1, name: "Yuriy", gender: "male", age: 22 },
+        { id: 2, name: "Dimon", gender: "flame", age: 234 },
+    ]
+  },
+};
+</script>
