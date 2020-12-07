f1115cdbe861338ba22dedf1e74ebdb73413a2e1
hel1yeah <hel1_yeah@ukr.net>
2020-12-08 00:39:02 +0200

    96 lesson

--- usersCRM/src/pages/Users.vue
+++ usersCRM/src/pages/Users.vue
@@ -2,48 +2,53 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-
         <!-- table -->
         <table>
-
-        <!-- head -->
+          <!-- head -->
           <thead>
             <tr>
-            <th>Имя</th>
-            <th>Возраст</th>
-            <th>Пол</th>
+              <th>Имя</th>
+              <th>Город</th>
+              <th>Телефон</th>
             </tr>
           </thead>
 
           <!-- body -->
           <tbody>
             <tr v-for="user in users" :key="user.id">
-            <td> {{user.name}} </td>
-            <td> {{user.age}} </td>
-            <td> {{user.gender}} </td>
+              <td>{{ user.name }}</td>
+              <td>{{ user.address.city }}</td>
+              <td>{{ user.phone }}</td>
             </tr>
           </tbody>
         </table>
-
       </div>
     </section>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-      users: [
-        
-      ],
+      users: [],
     };
   },
   created() {
-    this.users = [
-      { id: 1, name: "Yuriy", gender: "male", age: 22 },
-        { id: 2, name: "Dimon", gender: "flame", age: 234 },
-    ]
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+        .then(response => {
+          console.log(response.data);
+          this.users = response.data
+        }).catch(error => {
+          console.log(error);
+        });
+
+    // this.users = [
+    //   { id: 1, name: "Yuriy", gender: "male", age: 22 },
+    //     { id: 2, name: "Dimon", gender: "flame", age: 234 },
+    // ]
   },
 };
 </script>
