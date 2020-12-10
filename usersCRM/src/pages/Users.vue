64aca82a6a5ec8d786a924fcb55e2f8bc1945932
hel1yeah <hel1_yeah@ukr.net>
2020-12-10 22:30:11 +0200

    98 added sort

--- usersCRM/src/pages/Users.vue
+++ usersCRM/src/pages/Users.vue
@@ -2,22 +2,23 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
+        <p>debug: sort: {{ currentSort }}, dir : {{ currentSortDir }}</p>
         <!-- table -->
         <table>
           <!-- head -->
           <thead>
             <tr>
-              <th>Имя</th>
-              <th>Город</th>
-              <th>Телефон</th>
+              <th @click="sort('name')">Имя</th>
+              <th @click="sort('website')">Город</th>
+              <th @click="sort('phone')">Телефон</th>
             </tr>
           </thead>
 
           <!-- body -->
           <tbody>
-            <tr v-for="user in users" :key="user.id">
+            <tr v-for="user in usersSort" :key="user.id">
               <td>{{ user.name }}</td>
-              <td>{{ user.address.city }}</td>
+              <td>{{ user.website }}</td>
               <td>{{ user.phone }}</td>
             </tr>
           </tbody>
@@ -33,22 +34,40 @@ export default {
   data() {
     return {
       users: [],
+      currentSort: "name",
+      currentSortDir: "asc",
     };
   },
   created() {
     axios
       .get("https://jsonplaceholder.typicode.com/users")
-        .then(response => {
-          console.log(response.data);
-          this.users = response.data
-        }).catch(error => {
-          console.log(error);
-        });
-
-    // this.users = [
-    //   { id: 1, name: "Yuriy", gender: "male", age: 22 },
-    //     { id: 2, name: "Dimon", gender: "flame", age: 234 },
-    // ]
+      .then((response) => {
+        this.users = response.data;
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
+  computed: {
+    usersSort () {
+      return this.users.sort((a, b) => {
+        let mod = 1
+        if (this.currentSortDir === 'desc') mod = -1
+        if (a[this.currentSort] < b[this.currentSort]) return -1 * mod
+        if (a[this.currentSort] > b[this.currentSort]) return 1 * mod
+        return 0
+      })
+      console.log(this.user.address.city);
+    }
+  },
+  methods: {
+    sort (e) {
+      if (e === this.currentSort) {
+        this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc'
+      }
+      this.currentSort = e
+    }
+  }
 };
 </script>
