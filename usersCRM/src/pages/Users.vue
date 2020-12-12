93233d6b59b0f5250a6f63d8f344c24df2be2f25
hel1yeah <hel1_yeah@ukr.net>
2020-12-12 19:03:01 +0200

    pagination

--- usersCRM/src/pages/Users.vue
+++ usersCRM/src/pages/Users.vue
@@ -2,15 +2,15 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-        <p>debug: sort: {{ currentSort }}, dir : {{ currentSortDir }}</p>
+        <p>debug: sort: {{ currentSort }}, dir : {{ currentSortDir }}, page : {{page.current}}, page : {{page.length}}</p>
         <!-- table -->
         <table>
           <!-- head -->
           <thead>
             <tr>
-              <th @click="sort('name')">Имя</th>
-              <th @click="sort('website')">Город</th>
-              <th @click="sort('phone')">Телефон</th>
+              <th @click="sort('name')">Имя &#8595</th>
+              <th @click="sort('website')">Емейл &#8595</th>
+              <th @click="sort('phone')">Телефон &#8595</th>
             </tr>
           </thead>
 
@@ -25,17 +25,34 @@
         </table>
       </div>
     </section>
+    <section>
+    <div class="container">
+      <div class="button-list">
+        <div class="btn btnPrimary" @click="prevPage">
+        &#8592
+        </div>
+        <div class="btn btnPrimary" @click="nextPage">
+        &#8594
+        </div>
+      </div>
+    </div>
+    </section>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+
 export default {
   data() {
     return {
       users: [],
       currentSort: "name",
-      currentSortDir: "asc",
+      currentSortDir: "abc",
+      page: {
+        current: 1,
+        length: 3,
+      },
     };
   },
   created() {
@@ -43,31 +60,56 @@ export default {
       .get("https://jsonplaceholder.typicode.com/users")
       .then((response) => {
         this.users = response.data;
-        console.log(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
   },
   computed: {
-    usersSort () {
-      return this.users.sort((a, b) => {
-        let mod = 1
-        if (this.currentSortDir === 'desc') mod = -1
-        if (a[this.currentSort] < b[this.currentSort]) return -1 * mod
-        if (a[this.currentSort] > b[this.currentSort]) return 1 * mod
-        return 0
-      })
-      console.log(this.user.address.city);
-    }
+    usersSort() {
+      return this.users
+        .sort((a, b) => {
+          let mod = 1;
+          if (this.currentSortDir === "cba") mod = -1;
+          if (a[this.currentSort] < b[this.currentSort]) return -1 * mod;
+          if (a[this.currentSort] > b[this.currentSort]) return 1 * mod;
+          return 0;
+        })
+        .filter((row, index) => {
+          let start = (this.page.current - 1) * this.page.length;
+          let end = this.page.current * this.page.length;
+          if (index >= start && index < end) return true;
+        });
+    },
   },
   methods: {
-    sort (e) {
+    sort(e) {
       if (e === this.currentSort) {
-        this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc'
+        this.currentSortDir = this.currentSortDir === "abc" ? "cba" : "abc";
       }
-      this.currentSort = e
-    }
-  }
+      this.currentSort = e;
+    },
+    // Pagination
+    prevPage() {
+      if (this.page.current > 1) this.page.current -= 1;
+    },
+    nextPage() {
+      if (this.page.current * this.page.length < this.users.length)
+        this.page.current += 1;
+    },
+    // Pagination
+
+  },
 };
 </script>
+
+<style lang="scss">
+.button-list {
+  width: 100%;
+  text-align: center;
+}
+.btn {
+  border-radius: 60px;
+  margin: 0 20px;
+}
+</style>
