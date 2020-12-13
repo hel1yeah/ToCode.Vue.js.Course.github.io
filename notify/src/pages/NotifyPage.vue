ad4f83b0db53e622ffd258d45f3578f6421bc32f
hel1yeah <hel1_yeah@ukr.net>
2020-12-13 21:36:17 +0200

    105 lessons, added messges in store

--- notify/src/pages/NotifyPage.vue
+++ notify/src/pages/NotifyPage.vue
@@ -6,12 +6,41 @@
         <div class="notify__wrapper">
           <!-- title -->
           <div class="notify__title">
-            <h3>Notify App</h3>
+            <p>Notify App</p>
+            <svg
+              @click="getLazyNotify"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              width="20px"
+              height="20px"
+              cursor="pointer"
+              x="0px"
+              y="0px"
+              viewBox="0 0 489.935 489.935"
+              style="enable-background: new 0 0 489.935 489.935"
+              xml:space="preserve"
+            >
+              <g>
+                <path
+                  d="M278.235,33.267c-116.7,0-211.6,95-211.6,211.7v0.7l-41.9-63.1c-4.1-6.2-12.5-7.9-18.7-3.8c-6.2,4.1-7.9,12.5-3.8,18.7 l60.8,91.5c2.2,3.3,5.7,5.4,9.6,5.9c0.6,0.1,1.1,0.1,1.7,0.1c3.3,0,6.5-1.2,9-3.5l84.5-76.1c5.5-5,6-13.5,1-19.1 c-5-5.5-13.5-6-19.1-1l-56.1,50.7v-1c0-101.9,82.8-184.7,184.6-184.7s184.7,82.8,184.7,184.7s-82.8,184.7-184.6,184.7 c-49.3,0-95.7-19.2-130.5-54.1c-5.3-5.3-13.8-5.3-19.1,0c-5.3,5.3-5.3,13.8,0,19.1c40,40,93.1,62,149.6,62 c116.6,0,211.6-94.9,211.6-211.7S394.935,33.267,278.235,33.267z"
+                ></path>
+              </g>
+            </svg>
           </div>
-          <!-- content -->
+
+          <!-- notify -->
           <div class="notify__content">
+            <!-- preloader -->
+            <preloader v-if="loading" :width="90" :height="90" />
+
+            <!-- erorr -->
+            <div class="error" v-if="error">
+              <p>{{ error }}</p>
+            </div>
+
             <!-- notify -->
-            <notify :messages="messages"> </notify>
+            <notify v-if="!loading && !error" :messages="messages" />
           </div>
         </div>
       </div>
@@ -22,24 +51,52 @@
 
 
 <script>
+import axios from "axios";
 import notify from "@/components/Notify.vue";
+// UI
+import preloader from '@/components/UI/Preloader.vue'
 
 export default {
   components: {
-    notify,
+    notify,preloader,
   },
   data() {
     return {
-      messages: [
-        { title: "message 1" },
-        { title: "message 2" },
-        { title: "message 3" },
-        { title: "message 4" },
-        { title: "message 5" },
-        { title: "message 6" },
-        { title: "message 7" },
-      ],
+      loading: false,
+      error: null,
     };
+  },
+  mounted() {
+    this.getLazyNotify();
+  },
+  computed: {
+    messages(){
+      return this.$store.getters.getMessage
+    }
+  },
+  created() {},
+  methods: {
+    getLazyNotify() {
+      this.loading = true;
+      setTimeout(() => {
+        this.getNotify();
+      }, 1800);
+    },
+    getNotify() {
+      this.loading = true;
+      axios
+        .get("https://tocode.ru/static/c/vue-pro/notifyApi.php")
+        .then((response) => {
+          let res = response.data.notify;
+          this.$store.dispatch('setMessage', res)
+          // this.messages = res;
+          console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => (this.loading = false));
+    },
   },
 };
 </script>
@@ -47,7 +104,7 @@ export default {
 
 
 <style lang="scss" scoped>
-@import '../assets/scss/main';
+@import "../assets/scss/main";
 .container {
   display: flex;
   align-items: center;
@@ -57,7 +114,7 @@ export default {
 
 .notify__wrapper {
   width: 400px;
-  background-color:$default-color;
+  background-color: $default-color;
   padding: 30px;
   border-radius: 16px;
   box-shadow: 0 12px 22px 0 rgba(0, 0, 0, 0.1);
--- notify/src/store/notify.js
+++ notify/src/store/notify.js
@@ -1,44 +1,20 @@
-import loadMore from '../assets/js/loadMore.js'
-
 export default {
   state: {
-    messages: [],
-    messagesMain: []
+    messages: []
   },
   mutations: {
-    setMessage (state, payload) {
-      state.messages = payload
-    },
-    setMessageMain (state, payload) {
-      state.messagesMain = payload
-    },
-    loadMessages (state, payload) {
-      state.messagesMain = [...state.messagesMain, ...payload]
+    setMessage( state, payload) {
+      state.messages = payload;
     }
   },
   actions: {
-    setMessage ({commit}, payload) {
+    setMessage({commit}, payload){
       commit('setMessage', payload)
-    },
-    setMessageMain ({commit}, payload) {
-      commit('setMessageMain', payload)
-    },
-    loadMessages ({commit, getters}) {
-      let res = getters.getMessageFilter
-      commit('loadMessages', loadMore(res))
     }
   },
   getters: {
-    getMessage (state) {
+    getMessage (state){
       return state.messages
-    },
-    getMessageFilter (state) {
-      return state.messages.filter(mes => {
-        return mes.main === false
-      })
-    },
-    getMessageMain (state) {
-      return state.messagesMain
     }
-  }
+  },
 }
