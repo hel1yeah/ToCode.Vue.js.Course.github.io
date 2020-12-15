084ec9e3145fa2947f2e38017d404450b1ccc8e4
hel1yeah <hel1_yeah@ukr.net>
2020-12-15 23:15:20 +0200

    added nuxt.js create new folder blog

---
+++ blog/pages/posts/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="posts">
+  <input type="text" v-model="postId">
+  <button @click="loadPost">get post</button>
+    <h1>user id is {{$route.params.id}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      postId: '',
+    }
+  },
+  methods: {
+    loadPost(){
+      this.$router.push(`/posts/${this.postId}`)
+    }
+  },
+}
+</script>
