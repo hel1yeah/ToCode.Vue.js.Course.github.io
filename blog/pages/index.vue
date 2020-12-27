2cacc5a40bfceca2a19c4b745cc606dfee6bf712
hel1yeah <hel1_yeah@ukr.net>
2020-12-27 13:56:17 +0200

    asyncData

--- blog/pages/index.vue
+++ blog/pages/index.vue
@@ -18,31 +18,31 @@ export default {
     promo,
     contacts,
   },
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          posts : [
+              {
+                id: 1,
+                title: "1 post",
+                descr:
+                  "Напоивший прямо рекламных там своих большой семь послушавшись города. Бросил его переписали напоивший имени.",
+                img:
+                  "https://images.wallpaperscraft.ru/image/avtomobil_neon_chelovek_137624_2560x1080.jpg",
+              }
+            ]
+          })
+      }, 1500);
+    }).then(data => {
+      return data
+    }).catch(e => {
+      context.error(e)
+    })
+  },
   data() {
     return {
-      posts: [
-        {
-          id: 1,
-          title: "1 post",
-          descr:
-            "Напоивший прямо рекламных там своих большой семь послушавшись города. Бросил его переписали напоивший имени.",
-          img: 'https://images.wallpaperscraft.ru/image/avtomobil_neon_chelovek_137624_2560x1080.jpg',
-        },
-        {
-          id: 2,
-          title: "2 post",
-          descr:
-            "Напоивший прямо рекламных там своих большой семь послушавшись города. Бросил его переписали напоивший имени.",
-          img: 'https://images.wallpaperscraft.ru/image/neon_ieroglify_nadpis_190976_1920x1080.jpg',
-        },
-        {
-          id: 3,
-          title: "3 post",
-          descr:
-            "Напоивший прямо рекламных там своих большой семь послушавшись города. Бросил его переписали напоивший имени.",
-          img: 'https://images.wallpaperscraft.ru/image/avtomobil_sportkar_neon_145748_2560x1080.jpg',
-        },
-      ],
+      posts: [],
     };
   },
 };
